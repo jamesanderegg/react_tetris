9228702ca0d964c64e0dca50c1e9ce9b5363d5ad
Juicyjames <iceandjames@gmail.com>
2020-03-14 21:46:31 -0600

    added functions to tetris file to handle movement and game start and reset.

--- react-tetris/src/components/Tetris.js
+++ react-tetris/src/components/Tetris.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import { createStage } from '../gameHelpers'
+
 //Style components
 import { StyledTetrisWrapper, StyledTetris} from './styles/StyledTetris';
 
@@ -22,8 +24,38 @@ const Tetris = () => {
     const [stage, setStage] = useStage(player);
 
     console.log('re-render')
+
+    const movePlayer = dir => {
+        updatePlayerPos({ x: dir, y: 0})
+    }
+
+    const startGame = () => {
+        //reset everything
+        setStage(createStage())
+        resetPlayer();
+    }
+
+    const drop = () => {
+        updatePlayerPos({ x:0, y: 1, collided: false})
+    }
+
+    const dropPlayer = () => {
+        drop();
+    }
+
+    const move = ({ keyCode }) => {
+        if(!gameOver){
+            if(keyCode === 37){
+                movePlayer(-1);
+            } else if (keyCode === 39){
+                movePlayer(1);
+            } else if (keyCode === 40 ){
+                dropPlayer();
+            }
+        }
+    }
     return (
-        <StyledTetrisWrapper>
+        <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
             <StyledTetris>
                 <Stage stage={stage}/>
                 <aside>
@@ -37,7 +69,7 @@ const Tetris = () => {
                         <Display text= "Level" />
                     </div>
                     )}
-                    <StartButton />
+                    <StartButton onClick= {startGame}/>
                 </aside>
             </StyledTetris>
         </StyledTetrisWrapper>
