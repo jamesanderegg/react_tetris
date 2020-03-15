11fe14811fd185faa6035a7b8a5d2228975ca94d
Juicyjames <iceandjames@gmail.com>
2020-03-15 11:54:02 -0600

    Holding down arrow keyCode 40 will now keep registering till it's let go.

--- react-tetris/src/components/Tetris.js
+++ react-tetris/src/components/Tetris.js
@@ -6,6 +6,7 @@ import { createStage, checkCollision } from '../gameHelpers'
 import { StyledTetrisWrapper, StyledTetris} from './styles/StyledTetris';
 
 //custom hooks
+import { useInterval } from '../hooks/useInterval'
 import {usePlayer} from '../hooks/usePlayer';
 import {useStage } from '../hooks/useStage';
 
@@ -33,6 +34,7 @@ const Tetris = () => {
     const startGame = () => {
         //reset everything
         setStage(createStage())
+        setDropTime(1000);
         resetPlayer();
         setGameOver(false);
     }
@@ -50,7 +52,16 @@ const Tetris = () => {
         }
     }
 
+    const keyUp = ({ keyCode }) => {
+        if(!gameOver){
+            if(keyCode === 40){
+                setDropTime(1000);
+            }
+        }
+    }
+
     const dropPlayer = () => {
+        setDropTime(null);
         drop();
     }
 
@@ -67,8 +78,18 @@ const Tetris = () => {
             }
         }
     }
+
+useInterval(() => {
+    drop();
+}, dropTime)
+
     return (
-        <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
+        <StyledTetrisWrapper 
+            role="button" 
+            tabIndex="0" 
+            onKeyDown={e => move(e)} 
+            onKeyUp={keyUp}
+        >
             <StyledTetris>
                 <Stage stage={stage}/>
                 <aside>
