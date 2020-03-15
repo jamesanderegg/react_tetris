684909cd193a0cfccf61cef79acec8ad57575492
Juicyjames <iceandjames@gmail.com>
2020-03-14 22:47:50 -0600

    collision detection added and tetrimnio will stack if at bottom or merge to the stage.

--- react-tetris/src/hooks/useStage.js
+++ react-tetris/src/hooks/useStage.js
@@ -1,34 +1,37 @@
 import { useState, useEffect } from 'react';
-
 import { createStage } from '../gameHelpers';
 
 export const useStage = (player, resetPlayer) => {
-    const [stage, setStage] = useState(createStage());
+  const [stage, setStage] = useState(createStage());
 
-    useEffect(() => {
-        const updateStage = prevStage => {
-            // we need to clear from previous renger
-            const newStage = prevStage.map(row =>
-                row.map(cell => (cell[1] === 'clear' ? [0, ' clear'] : cell )),
-                );
-            //then draw the tetromino
-            player.tetromino.forEach((row,y) => {
-                row.forEach((value, x) => {
-                    if(value !== 0){
-                        newStage[y + player.pos.y][x + player.pos.x] = [
-                            value,
-                            `${player.collided ? 'merged' : 'clear'}`,
-                        ]
-                    }
-                })
-            });
+  useEffect(() => {
+    const updateStage = prevStage => {
+      // First flush the stage
+      const newStage = prevStage.map(row =>
+        row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
+      );
 
-            return newStage;
-        };
-        
-        setStage(prev => updateStage(prev))
+      // Then draw the tetromino
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if (value !== 0) {
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              `${player.collided ? 'merged' : 'clear'}`,
+            ];
+          }
+        });
+      });
+      // Then check if we collided
+      if (player.collided) {
+        resetPlayer();
+      }
 
-    }, [player]);
+      return newStage;
+    };
 
-    return [stage, setStage];
-}
+    setStage(prev => updateStage(prev));
+  }, [player, resetPlayer]);
+
+  return [stage, setStage];
+};
